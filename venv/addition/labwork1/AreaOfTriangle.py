a93fa0ae014c26ff4fdf8747b355d716610af5bf
kamalrai <nahrai123@gmail,com>
2021-02-25 09:49:59 +0545

    name

---
+++ venv/addition/labwork1/AreaOfTriangle.py
@@ -0,0 +1,9 @@
+''#write a program that reads the length of the base and the height of a right angled triangle and prints the area. Every
+#number is given on a seperate.
+
+base = int(input("enter the length of the base: "))
+height = int(input("enter the length of the height: "))
+
+area = base*height//2
+
+print(f"the area of the right angled traingle is {area}" )
---
+++ venv/addition/labwork1/sumOfNumbers.py
@@ -0,0 +1,8 @@
+#write a program that takes three numbers and prints their sum. Every number is given on a seperate line.
+
+num1 = int(input("enter the first number: "))
+num2 = int(input("enter the second number: "))
+num3 = int(input("enter the third number: "))
+sum = num1 + num2 +num3
+
+print(f"the sum of numbers is {sum}")
